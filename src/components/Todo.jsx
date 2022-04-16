4026b1bfb99f79404dc5d5cc6af3dfbe00655b09
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-16 16:33:03 +0200

    finished the example

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,16 +5,24 @@ function Todo() {
   const [loading, setLoading] = useState(true)
   const [todo, setTodo] = useState({})
 
+  const isMounted = useRef(true)
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then ((res) => res.json())
     .then((data) => {
       setTimeout(() => {
-        setTodo(data)
-        setLoading(false)
+        if(isMounted.current) {
+          setTodo(data)
+          setLoading(false)
+        }
       }, 3000)
     })
-  }, [])
+    // This runs when the component is unmounted
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
   return ( loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>)
 }
 
