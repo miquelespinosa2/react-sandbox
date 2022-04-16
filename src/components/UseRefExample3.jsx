c50187dba13185435149cf822d110eff70c6c615
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-16 16:30:34 +0200

    created the memory leak

--- src/components/UseRefExample3.jsx
+++ src/components/UseRefExample3.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Todo from './Todo'
+import {useState} from 'react'
 
 function UseRefExample3() {
+  const [showTodo, setShowTodo] = useState(true)
   return (
-    <div>UseRefExample3</div>
+    <div>
+      {showTodo && <Todo />}
+      <button className="btn btn-primary" onClick={() => setShowTodo(!showTodo)}>Toggle Todo</button>
+    </div>
   )
 }
 
