6e043c1e0b0889cf02e9fbd67b400074df3bb12c
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-04-16 16:38:46 +0200

    created the useMemo

--- src/components/UseMemoExample.jsx
+++ src/components/UseMemoExample.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {useState, useEffect, useRef, useMemo} from 'react'
 
 function UseMemoExample() {
+  const [number, setNumber] = useState(1)
+  const [inc, setInc] = useState(0)
+
+
+  const renders = useRef(1)
+
+  useEffect(() => {
+    renders.current = renders.current +1
+  })
+
+
+
   return (
-    <div>UseMemoExample</div>
+    <div>
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)}className="form-control w-25" />
+      <h3>Renders: {renders.current}</h3>
+    </div>
   )
 }
 
